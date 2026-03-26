c5b03d99ba1ccfbbef5804cd907cb957bebb6467
Esteban Méndez <mendezesteban1804@gmail.com>
2026-03-26 14:13:28 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,20 @@
+variable "location" {
+  default = "westeurope"
+}
 
+variable "resource_group_name" {
+  default = "rg-terraform-demo"
+}
+
+variable "vm_name" {
+  default = "vm-demo"
+}
+
+variable "admin_username" {
+  default = "azureuser"
+}
+
+variable "admin_password" {
+  description = "VM password"
+  sensitive   = true
+}
